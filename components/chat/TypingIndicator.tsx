44645ba7dea0f779614af936eac63e11e5a6c6e0
오주현 <dhwngus99@gmail.com>
2026-02-22 11:08:07 +0900

    Revert "초기 퍼블리싱"
    This reverts commit fcca1dc3cb64a131e9575afd0de75958739e3c16.

--- components/chat/TypingIndicator.tsx
+++
@@ -1,9 +0,0 @@
-export function TypingIndicator() {
-  return (
-    <div className="flex space-x-2 p-4 card-flat w-fit animate-fade-in">
-      <div className="w-2 h-2 bg-primary rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-      <div className="w-2 h-2 bg-primary rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-      <div className="w-2 h-2 bg-primary rounded-full animate-bounce"></div>
-    </div>
-  );
-}
